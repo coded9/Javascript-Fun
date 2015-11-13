3725714bcdcdb92ebd8f7003772d190e675cfbdc
Ashok Borra <coded9@users.noreply.github.com>
2015-11-13 04:16:50 -0600

    Update location.js

--- Knowing-your-location/location.js
+++ Knowing-your-location/location.js
@@ -1,14 +1,13 @@
-
-<script>
-  
-  if(navigator.geolocation){
+ if(navigator.geolocation){
+   
     navigator.geolocation.getCurrentPosition(function(position){
       document.getElementById("displayHere").innerHTML = "latitude: "+position.coords.latitude+"<br>longitude:"+position.coords.longitude;
+      var myPos = position.coords.latitude+","+position.coords.longitude;
+  console.log(myPos);
+      var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+myPos+"&zoom=18&size=400x400&sensor=false";
+document.getElementById("imageMap").src= img_url;
   });
-}
-var myPos = position.coords.latitude+","+position.coords.longitude;
-var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="+myPos+"&zoom=18&size=400x400&sensor=false"
-document.getElementById("imageMap").src =img_url;
-    
-  
-</script>
+
+
+
+   }
