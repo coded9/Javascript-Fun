37a3fdcd926cd3b18713527b29b10fd2e17654b4
Ashok Borra <coded9@users.noreply.github.com>
2015-11-13 03:45:10 -0600

    Update location.js

--- Knowing-your-location/location.js
+++ Knowing-your-location/location.js
@@ -6,5 +6,9 @@
       document.getElementById("displayHere").innerHTML = "latitude: "+position.coords.latitude+"<br>longitude:"+position.coords.longitude;
   });
 }
+var myPos = position.coords.latitude+","+position.coords.longitude;
+var img_url = "http://maps.googleapis.com/maps/api/staticmap?center=
+    "+latlon+"&zoom=18&size=400x400&sensor=false"
+    
   
 </script>
